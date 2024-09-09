06e043ec2f8177b3d01f07846bc3d1fbea885a18
riki08 <riki0885@gmail.com>
2024-09-09 08:35:09 -0500

    agregar metodo en el bloc para obtener las cards

--- lib/cars/presentation/cubit/card_cubit.dart
+++ lib/cars/presentation/cubit/card_cubit.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:hiberus/cars/domain/repositories/card_api_repository.dart';
 import 'package:hiberus/models/card_list_model.dart';
 import 'package:hiberus/utils/resources/base_cubit.dart';
+import 'package:hiberus/utils/resources/data_state.dart';
 
 part 'card_state.dart';
 
@@ -10,4 +11,17 @@ class CardCubit extends BaseCubit<CardState> {
   final CardApiRepository _apiRepository;
 
   CardCubit(this._apiRepository) : super(const CardInitial());
+
+  Future<void> getAllCards() async {
+    if (isBusy) return;
+    await run(() async {
+      emit(CardLoading());
+      final response = await _apiRepository.getAllCards();
+
+      if (response is DataSuccess) {
+      } else if (response is DataFailed) {
+        emit(CardFailed(error: response.error));
+      }
+    });
+  }
 }
